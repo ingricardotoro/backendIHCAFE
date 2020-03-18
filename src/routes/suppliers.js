f543726d14be95527ae678fc623affb11e8f151b
marvinlin <marvinlink07@gmail.com>
2020-03-17 22:56:12 -0600

    delete src

--- src/routes/suppliers.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-const router = Router()
-
-import {ListSuppliers} from '../controllers/suppliersController'
-
-router.get('/',ListSuppliers); // para poder listar todos los proveedores
-
-export default router;
