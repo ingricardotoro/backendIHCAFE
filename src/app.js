5aed63c0d8610a7df155f5f165103631ce1f7323
marvinlin <marvinlink07@gmail.com>
2020-03-21 21:28:22 -0600

    port 4000

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ import filesRoutes from './routes/files'
 const app = express();
 
 //settings
-app.set('port',process.env.PORT || 5000);
+app.set('port',process.env.PORT || 4000);
 
 //middlewares
 app.use(morgan('dev'));
