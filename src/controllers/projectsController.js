19a33ba00ad4433bb2093de95bd3c7163f802c6d
marvinlin <marvinlink07@gmail.com>
2020-04-03 12:03:47 -0600

    test

--- src/controllers/projectsController.js
+++ src/controllers/projectsController.js
@@ -64,12 +64,13 @@ export async function deleteProject(req,res){
 
 export async function createProjects(req, res){
 
-    const { code,name,description,startdate,enddate,status,location,budget_id,team_id,priority } = req.body;
+    const { code,name,description,,startdate,enddate,status,location,budget_id,team_id,priority } = req.body;
     try {
         let newProject = await Project.create({
             code ,
             name ,
             description ,
+            budgetstart,
             startdate ,
             enddate ,
             status ,
@@ -79,7 +80,7 @@ export async function createProjects(req, res){
             priority 
             
         },{
-            fields:['code','name','description','startdate','enddate','status','location','budget_id','team_id','priority']
+            fields:['code','name','description','budgetstart','startdate','enddate','status','location','budget_id','team_id','priority']
         });
 
         if (newProject){
