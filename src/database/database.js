fa11863ae173342878827297ca9c47687a6180a0
marvinlin <marvinlink07@gmail.com>
2020-04-19 23:28:46 -0600

    test

--- src/database/database.js
+++ src/database/database.js
@@ -1,20 +1,20 @@
-require('dotenv').config();
-import Sequelize from 'sequelize';
+require("dotenv").config();
+import Sequelize from "sequelize";
 
 //create cadena de conexion
 export const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host:process.env.DB_URI,
-        dialect: 'postgres',
-        pool:{
-            max:5,
-            min:0,
-            require:30000,
-            idle:10000
-        },
-        //logging:false // para ver las respuestas de la bd por consola
-    }
-);  
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_URI,
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      require: 30000,
+      idle: 10000,
+    },
+    logging: true, // para ver las respuestas de la bd por consola
+  }
+);
