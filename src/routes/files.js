9afc9ac6e24d3307b21b283a69ed2f13a64fdfcc
marvinlin <marvinlink07@gmail.com>
2020-04-22 11:15:02 -0600

    test

--- src/routes/files.js
+++ src/routes/files.js
@@ -21,7 +21,7 @@ var storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5000000 }, //5 megas
+  limits: { fileSize: 20000000 }, //5 megas
   fileFilter: (req, file, cb) => {
     const filetypes = /jpeg|jpg|png|gif|pdf/;
     const mimetype = filetypes.test(file.mimetype);
