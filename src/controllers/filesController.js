1371a9d82ccb1e9c4539f701163a0ea776b1b349
marvinlin <marvinlink07@gmail.com>
2020-04-22 03:03:30 -0600

    list

--- src/controllers/filesController.js
+++ src/controllers/filesController.js
@@ -1,44 +1,38 @@
-import Archivo from '../models/Archivo'
+import Archivo from "../models/Archivo";
 
-export async function FileByBudgetId(req, res){
+//buscamos los archivo que pertenecen a este budgetLine
+export async function FileByBudgetId(req, res) {
+  const { id } = req.params;
 
-    const { id } = req.params;
-   
-        try {
-       
-            const files =await  Archivo.findAll({
-               
-                where:{
-                    budgetlineatlas_id:id
-                }
-            });
-            res.json({
-                files
-            })
-          
-        } catch (error) {
-            console.log("ERROR AL QUERER BUSCAR El ARCHIVO POR BUDGETLINE:"+error);
-        }
-    
-
+  try {
+    const files = await Archivo.findAll({
+      where: {
+        budgetlineatlas_id: id,
+      },
+    });
+    res.json({
+      files,
+    });
+  } catch (error) {
+    console.log("ERROR AL QUERER BUSCAR El ARCHIVO POR BUDGETLINE:" + error);
+  }
 }
 
 //funcion para eliminar archivos por id
-export async function DeleteFileById(req, res){
-
-    const { id } = req.params;
-    try {
-        const deleteRowCount = await Archivo.destroy({
-            where:{
-                id
-            }
-        });
-        res.json({
-           // message:"Archivo Eliminado Satifactoriamente",
-            //count:deleteRowCount
-            deleteRowCount
-        });
-    } catch (error) {
-        console.log("ERROR AL QUERE ELIMINAR EL Archivo:"+error);
-    }
+export async function DeleteFileById(req, res) {
+  const { filename } = req.params;
+  try {
+    const deleteRowCount = await Archivo.destroy({
+      where: {
+        filename,
+      },
+    });
+    res.json({
+      // message:"Archivo Eliminado Satifactoriamente",
+      //count:deleteRowCount
+      deleteRowCount,
+    });
+  } catch (error) {
+    console.log("ERROR AL QUERE ELIMINAR EL Archivo:" + error);
+  }
 }
