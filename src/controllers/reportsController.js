977fa7915f813baf23e6832fc9b32ea58d9c22c2
marvinlin <marvinlink07@gmail.com>
2020-04-24 01:35:39 -0600

    test

--- src/controllers/reportsController.js
+++ src/controllers/reportsController.js
@@ -1,8 +1,10 @@
-const pdf = require('html-pdf');
+//const pdf = require('html-pdf');
 import pdfTemplate from '../templates/report'
+import pdf from 'pdf-creator-node'
+import fs from 'fs'
 
 
-export async function createReports(req, res) {
+/* export async function createReports(req, res) {
     console.log(req.body)
     pdf.create(pdfTemplate(req.body), {}).toFile('Reportes.pdf', (err) => {
         if (err) {
@@ -15,22 +17,36 @@ export async function createReports(req, res) {
 
 export async function fetchPdf(req, res) {
     res.sendFile('${__dirname}/Reportes.pdf')
-}
+} */
 
 export async function reportepdf(req, res) {
 
-    const content = `
-        <h1>Título en el PDF creado con el paquete html-pdf</h1>
-        <p>Generando un PDF con un HTML sencillo</p>
-        `;
-
-    pdf.create(content).toFile('./html-pdf.pdf', function (err, res) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(res);
+    const html = fs.readFileSync(pdfTemplate, 'utf8');
+    var options = {
+        format: "A3",
+        orientation: "portrait",
+        border: "10mm",
+        header: {
+            height: "45mm",
+            contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
+        },
+        "footer": {
+            "height": "28mm",
+            "contents": {
+                first: 'Cover page',
+                2: 'Second page', // Any page number is working. 1-based index
+                default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+                last: 'Last Page'
+            }
         }
-    });
+    }
+    pdf.create(document, options)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(error => {
+            console.error(error)
+        });
 
 }
 
