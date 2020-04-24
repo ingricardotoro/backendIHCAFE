d254427b144a628fc0e17ea479adea8d9d3390d8
marvinlin <marvinlink07@gmail.com>
2020-04-24 00:45:47 -0600

    test

--- src/controllers/reportsController.js
+++ src/controllers/reportsController.js
@@ -17,3 +17,20 @@ export async function fetchPdf(req, res) {
     res.sendFile('${__dirname}/Reportes.pdf')
 }
 
+export async function reportepdf(req, res) {
+
+    const content = `
+        <h1>Título en el PDF creado con el paquete html-pdf</h1>
+        <p>Generando un PDF con un HTML sencillo</p>
+        `;
+
+    pdf.create(content).toFile('./html-pdf.pdf', function (err, res) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(res);
+        }
+    });
+
+}
+
