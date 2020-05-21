6424a3c9fd42416eda007f93e8398808d71b89fe
marvinlin <marvinlink07@gmail.com>
2020-05-21 14:55:52 -0600

    users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 const router = Router();
 
-import { register, listUsers } from "../controllers/usersController";
+import { register, listUsers, login } from "../controllers/usersController";
 
 
 router.get("/", listUsers);
 router.post("/register", register);
+router.post("/login", login);
 
 
 export default router;
