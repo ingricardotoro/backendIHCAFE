c275c271d9a813905683ef7812b2526cc069702a
marvinlin <marvinlink07@gmail.com>
2020-05-21 15:43:30 -0600

    test

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -34,7 +34,7 @@ export async function login(req, res) {
             if (user) {
                 if (bcrypt.compareSync(req.body.password, user.password)) {
                     console.log("CLAVE OK:")
-                    console.log("dataValues:" + user.dataValues)
+                    console.log("dataValues:" + user.data)
                     let token = jwt.sign(user.data, process.env.SECRET_KEY, {
                         expiresIn: 1440
                     })
