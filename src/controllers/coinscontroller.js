abf64ca453f1f2aa1508cf4650bd182189c06c44
marvinlin <marvinlink07@gmail.com>
2020-05-30 23:35:22 -0600

    fix

--- src/controllers/coinscontroller.js
+++ src/controllers/coinscontroller.js
@@ -11,3 +11,64 @@ export async function Listcoins(req, res) {
     console.log("ERROR AL QUERE LISTAR La monedas:" + error);
   }
 }
+
+
+export async function createCoin(req, res) {
+  const {
+    name,
+    description,
+    code,
+  } = req.body;
+  try {
+    let newCoin = await Coin.create(
+      {
+        name,
+        description,
+        code
+      },
+      {
+        fields: [
+          "name",
+          "description",
+          "code"
+        ],
+      }
+    );
+
+    if (newCoin) {
+      return res.json({
+        message: "Moneda Creada Exitosamente",
+        data: newCoin,
+      });
+    } else {
+      return res.json({
+        message: "No se Pudo Crear Nueva Moneda",
+        data: {},
+      });
+    }
+  } catch (error) {
+    //console.log(error);
+    res.status(500).json({
+      message: "Error al crear nuevas Moneda",
+      data: {},
+    });
+  }
+}
+
+
+export async function deleteCoin(req, res) {
+  const { id } = req.params;
+  try {
+    const deleteRowCount = await Coin.destroy({
+      where: {
+        id,
+      },
+    });
+    res.json({
+      message: "Moneda Eliminada Satifactoriamente",
+      count: deleteRowCount,
+    });
+  } catch (error) {
+    console.log("ERROR AL QUERE ELIMINAR Moneda:" + error);
+  }
+}
--- src/routes/coins.js
+++ src/routes/coins.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 const router = Router();
 
-import { Listcoins } from "../controllers/coinscontroller";
+import { Listcoins, createCoin, deleteCoin } from "../controllers/coinscontroller";
 
-//router.post("/api/coins", CreateCoins);
+router.post("/", createCoin);
+router.post("/delete/:id", deleteCoin);
 router.get("/", Listcoins);
-
-/*router.get('/categories_childs/:id',categories_childs);// para obtener todas las categorias hijas dado el id del padre
-router.get('/child/:id',childbyid); // para solicitar una clasificacion hijo en especifico*/
 
 export default router;
