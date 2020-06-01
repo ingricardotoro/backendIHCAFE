df61ebe2f15b77586053db71f20219d38c5297fc
marvinlin <marvinlink07@gmail.com>
2020-06-01 03:41:04 -0600

    fix

--- src/controllers/teammembersController.js
+++ src/controllers/teammembersController.js
@@ -34,3 +34,48 @@ export async function deleteTeammember(req, res) {
         console.log("ERROR AL QUERE ELIMINAR TeamMember:" + error);
     }
 }
+
+export async function createTeammember(req, res) {
+
+    const {
+        team_id,
+        person_id,
+        rol_id
+    } = req.body;
+
+    try {
+        let newTeamMember = await TeamMember.create(
+            {
+                team_id,
+                person_id,
+                rol_id
+            },
+            {
+                fields: [
+                    "team_id",
+                    "person_id",
+                    "rol_id"
+                ],
+            }
+        );
+
+        if (newTeamMember) {
+            return res.json({
+                message: "TeamMember Creado Exitosamente",
+                data: TeamMember,
+            });
+        } else {
+            return res.json({
+                message: "No se Pudo Crear Nuevo TeamMember",
+                data: {},
+            });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Error al crear nuevo TeamMember",
+            data: {},
+        });
+    }
+}
