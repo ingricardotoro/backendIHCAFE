d86667f9ef5e83f74ee63fd0c709309b2b42c8a1
marvinlin <marvinlink07@gmail.com>
2020-06-12 01:00:04 -0600

    fix

--- src/controllers/budgetLinesController.js
+++ src/controllers/budgetLinesController.js
@@ -578,7 +578,7 @@ export async function GraficaAtlasByProjectID(req, res) {
 
       /*group: [sequelize.fn('date_part', 'week', sequelize.col('date_start'))],*/
       /*order: sequelize.col("date_part('week', date_start)")*/
-      group: ['date_start']
+      group: ['week']
 
     });
     res.json({
