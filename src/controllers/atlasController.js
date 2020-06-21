2272b0650347c45cc474ef8ef48a73d97dce530c
marvinlin <marvinlink07@gmail.com>
2020-06-21 15:39:28 -0600

    fix

--- src/controllers/atlasController.js
+++ src/controllers/atlasController.js
@@ -3,14 +3,14 @@ import AtlasAccount from '../models/AtlasAccount'
 
 
 //codigo para obtener los RESULTADOS ATLAS , de codigo 0
-export async function atlas_resultados(req,res){
-    
+export async function atlas_resultados(req, res) {
+
     try {
-       
-        const atlas_resultados =await  Atlas.findAll({
-           
-            where:{
-                code_atlas:'0'
+
+        const atlas_resultados = await Atlas.findAll({
+
+            where: {
+                code_atlas: '0'
             },
             order: [
                 ['id', 'ASC']
@@ -19,48 +19,48 @@ export async function atlas_resultados(req,res){
         res.json({
             atlas_resultados
         })
-      
+
     } catch (error) {
-        console.log("ERROR AL QUERE LISTAR La Categorias de resultados de AtLAS:"+error);
+        console.log("ERROR AL QUERE LISTAR La Categorias de resultados de AtLAS:" + error);
     }
-   
-}  
+
+}
 
 //codigo para obtener los productos dado el codigo de resultado atlas
-export async function atlas_productos(req,res){
+export async function atlas_productos(req, res) {
     const { id } = req.params;
-        
-        try {
-       
-            const productos_atlas =await  Atlas.findAll({
-               
-                where:{
-                    code_atlas:id
-                } ,
-                
+
+    try {
+
+        const productos_atlas = await Atlas.findAll({
+
+            where: {
+                code_atlas: id
+            },
+
             order: [
                 ['id', 'ASC']
             ]
-            });
-            res.json({
-                productos_atlas
-            })
-          
-        } catch (error) {
-            console.log("ERROR AL QUERE LISTAR Los PRoductos ATLAS:"+error);
-        }
-   
+        });
+        res.json({
+            productos_atlas
+        })
+
+    } catch (error) {
+        console.log("ERROR AL QUERE LISTAR Los PRoductos ATLAS:" + error);
+    }
+
 }
 
 //codigo para obtener los RESULTADOS ATLAS , de codigo 0
-export async function atlas_accounts(req,res){
-    
+export async function atlas_accounts(req, res) {
+
     try {
-       
-        const atlas_accounts =await  AtlasAccount.findAll({
-           
-            where:{
-                code_atlas:'0'
+
+        const atlas_accounts = await AtlasAccount.findAll({
+
+            where: {
+                code_atlas: '0'
             },
             order: [
                 ['id', 'ASC']
@@ -69,31 +69,57 @@ export async function atlas_accounts(req,res){
         res.json({
             atlas_accounts
         })
-      
+
     } catch (error) {
-        console.log("ERROR AL QUERE LISTAR LAS CUENTAS de AtLAS:"+error);
+        console.log("ERROR AL QUERE LISTAR LAS CUENTAS de AtLAS:" + error);
     }
-   
-} 
+
+}
 
 //codigo para obtener las sub categorias de un id atlas dado
-export async function atlas_sub_accounts(req,res){
+export async function atlas_sub_accounts(req, res) {
     const { id } = req.params;
-        
-        try {
-       
-            const sub_accounts =await  AtlasAccount.findAll({
-               
-                where:{
-                    code_atlas:id
-                } 
-            });
-            res.json({
-                sub_accounts
-            })
-          
-        } catch (error) {
-            console.log("ERROR AL QUERE LISTAR Los PRoductos ATLAS:"+error);
-        }
-   
+
+    try {
+
+        const sub_accounts = await AtlasAccount.findAll({
+
+            where: {
+                code_atlas: id
+            }
+        });
+        res.json({
+            sub_accounts
+        })
+
+    } catch (error) {
+        console.log("ERROR AL QUERE LISTAR Las Sub categorias ATLAS:" + error);
+    }
+
+}
+
+
+
+/**  Para los reportes Atlas */
+
+//codigo para obtener Una de las sub categorias de un id atlas dado
+export async function find_sub_atlas_category(req, res) {
+    const { id } = req.params;
+
+    try {
+
+        const sub_accounts = await AtlasAccount.findOne({
+
+            where: {
+                id
+            }
+        });
+        res.json({
+            sub_accounts
+        })
+
+    } catch (error) {
+        console.log("ERROR AL QUERE LISTAR la Subcategoria ATLAS:" + error);
+    }
+
 }
--- src/routes/atlas.js
+++ src/routes/atlas.js
@@ -1,13 +1,24 @@
-import {Router} from 'express';
+import { Router } from 'express';
 const router = Router();
 
 
-import {atlas_resultados,atlas_productos,atlas_accounts,atlas_sub_accounts} from '../controllers/atlasController'
+import {
+    atlas_resultados,
+    atlas_productos,
+    atlas_accounts,
+    atlas_sub_accounts,
+    find_sub_atlas_category
 
-router.get('/resultados',atlas_resultados);// obtener los RESULTADOS ATLAS son de codigo 0
-router.get('/productos/:id',atlas_productos);// para obtener todas los producots hijos dado el codigo atlas del resultado
+} from '../controllers/atlasController'
 
-router.get('/accounts',atlas_accounts);// obtener las cuentas ATLAS
-router.get('/sub_accounts/:id',atlas_sub_accounts);// para obtener todas las sub categorias de una cuenta atlas dada
+router.get('/resultados', atlas_resultados);// obtener los RESULTADOS ATLAS son de codigo 0
+router.get('/productos/:id', atlas_productos);// para obtener todas los producots hijos dado el codigo atlas del resultado
+
+router.get('/accounts', atlas_accounts);// obtener las cuentas ATLAS
+router.get('/sub_accounts/:id', atlas_sub_accounts);// para obtener todas las sub categorias de una cuenta atlas dada
+
+/**Para los reportes atlas */
+router.get('/find_sub_atlas_category/:id', find_sub_atlas_category);// para obtener una de las sub categorias de una cuenta atlas dada
+
 
 export default router;
