d0d1d7a6b8c4bcae6bbba200e598927c1d3469b0
Marvin Toro <marvinlink07@gmail.com>
2022-01-24 20:07:13 -0600

    cors test

--- src/app.js
+++ src/app.js
@@ -1,68 +1,78 @@
-require("dotenv").config();
+require('dotenv').config();
 //importamos el objeto servidor
-import express, { json } from "express";
+import express, { json } from 'express';
 //morgan ayuda a recibir en consola las peticiones http
-import morgan from "morgan";
-import path from "path";
+import morgan from 'morgan';
+import path from 'path';
 //import multer from "multer"; // para subir archivos
 
 //importamos el middleare core para el enlace entre servidores
-import cors from "cors";
+import cors from 'cors';
+
+const whiteList = ['http://167.99.15.83'];
+
+const corsOptions = {
+    origin: function(origin, callback) {
+        if (whiteList.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not Allow by CORS'));
+        }
+    },
+};
 
 //Import Routes
-import projectRoutes from "./routes/projects";
-import budgetstRoutes from "./routes/budgets";
-import budgetLinesRoutes from "./routes/budgetslines";
-import categoriesRoutes from "./routes/categories";
-import teamsRoutes from "./routes/teams";
-import teamMembersRoutes from "./routes/teammembers";
-import rolesRoutes from "./routes/roles";
-import taskRoutes from "./routes/task";
-import accountsRoutes from "./routes/accounts";
-import atlasRoutes from "./routes/atlas";
-import suppliersRoutes from "./routes/suppliers";
-import filesRoutes from "./routes/files";
-import coinsRoutes from "./routes/coins";
+import projectRoutes from './routes/projects';
+import budgetstRoutes from './routes/budgets';
+import budgetLinesRoutes from './routes/budgetslines';
+import categoriesRoutes from './routes/categories';
+import teamsRoutes from './routes/teams';
+import teamMembersRoutes from './routes/teammembers';
+import rolesRoutes from './routes/roles';
+import taskRoutes from './routes/task';
+import accountsRoutes from './routes/accounts';
+import atlasRoutes from './routes/atlas';
+import suppliersRoutes from './routes/suppliers';
+import filesRoutes from './routes/files';
+import coinsRoutes from './routes/coins';
 import conversionsRoutes from './routes/conversions';
-import reportsRoutes from "./routes/reports";
-import usersRoutes from "./routes/users";
-import tipousersRoutes from "./routes/tipousers";
-
-
+import reportsRoutes from './routes/reports';
+import usersRoutes from './routes/users';
+import tipousersRoutes from './routes/tipousers';
 
 //Initialization
 const app = express();
 
 //settings
-app.set("port", process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4000);
 
 //middlewares
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors());
 app.use(json()); // para entender archivos json
 
 //routes
-app.use("/api/projects", projectRoutes);
-app.use("/api/budgets", budgetstRoutes);
-app.use("/api/budgetlines", budgetLinesRoutes);
-app.use("/api/categories", categoriesRoutes);
-app.use("/api/teams", teamsRoutes);
-app.use("/api/teammembers", teamMembersRoutes);
-app.use("/api/roles", rolesRoutes);
-app.use("/api/accounts", accountsRoutes);
-app.use("/api/atlas", atlasRoutes);
-app.use("/api/tasks", taskRoutes);
-app.use("/api/suppliers", suppliersRoutes);
-app.use("/api/files", filesRoutes);
-app.use("/api/coins", coinsRoutes);
-app.use("/api/conversions", conversionsRoutes);
-app.use("/api/reports", reportsRoutes);
-app.use("/api/users", usersRoutes);
-app.use("/api/tipousers", tipousersRoutes);
+app.use('/api/projects', cors(corsOptions), projectRoutes);
+app.use('/api/budgets', cors(corsOptions), budgetstRoutes);
+app.use('/api/budgetlines', cors(corsOptions), budgetLinesRoutes);
+app.use('/api/categories', cors(corsOptions), categoriesRoutes);
+app.use('/api/teams', cors(corsOptions), teamsRoutes);
+app.use('/api/teammembers', cors(corsOptions), teamMembersRoutes);
+app.use('/api/roles', cors(corsOptions), rolesRoutes);
+app.use('/api/accounts', cors(corsOptions), accountsRoutes);
+app.use('/api/atlas', cors(corsOptions), atlasRoutes);
+app.use('/api/tasks', cors(corsOptions), taskRoutes);
+app.use('/api/suppliers', cors(corsOptions), suppliersRoutes);
+app.use('/api/files', cors(corsOptions), filesRoutes);
+app.use('/api/coins', cors(corsOptions), coinsRoutes);
+app.use('/api/conversions', cors(corsOptions), conversionsRoutes);
+app.use('/api/reports', cors(corsOptions), reportsRoutes);
+app.use('/api/users', cors(corsOptions), usersRoutes);
+app.use('/api/tipousers', cors(corsOptions), tipousersRoutes);
 
 //crear carpeta publica para el navegador
 //app.use(express.static(path.join(__dirname,'public')));
-app.use(express.static(path.join(__dirname, "../src/public")));
+app.use(express.static(path.join(__dirname, '../src/public')));
 
 //app.use(express.static(__dirname + '../src/public'));
 //app.use(express.static('public'));
